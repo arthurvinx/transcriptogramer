c58e6f9fb6cff1b0a1f11782b2ee64c2bd175c4e
Diego Morais <arthur.vinx@gmail.com>
2018-05-04 15:34:01 -0300

    Add files via upload

--- R/package.R
+++ R/package.R
@@ -14,16 +14,20 @@
 #'
 #' Rybarczyk-Filho, J. L., Castro, M. A. A., Dalmolin, R. J. S., Moreira, J. C. F., Brunnet, L. G., and de Almeida, R. M. C. (2011). Towards a genome-wide tran-scriptogram: the Saccharomyces cerevisiae case. Nucleic Acids Research, 39(8), 3005-3016.
 #'
+#' Ferrareze, P. A. G., Streit, R. S. A., Santos, P. R. dos, Santos, F. M. dos, Almeida, R. M. C. de, Schrank, A., Kmetzsch, L., Vainstein, M. H. and Staats, C. C. (2017). Transcriptional Analysis Allows Genome Reannotation and Reveals that Cryptococcus gattii VGII Undergoes Nutrient Restriction during Infection. Microorganisms.
+#'
+#' de Almeida, R. M. C., Clendenon, S. G., Richards, W. G., Boedigheimer, M., Da-more, M., Rossetti, S., Harris, P. C., Herbert, B. S., Xu, W. M., Wandinger-Ness, A., Ward, H. H., Glazier, J. A. and Bacallao, R. L. (2016). Transcriptome analy-sis reveals manifold mechanisms of cyst development in ADPKD. Human Genomics, 10(1), 1–24.
+#'
 #' @seealso
 #' Bioconductor release: \url{https://doi.org/doi:10.18129/B9.bioc.transcriptogramer}
 #'
 #' Github: \url{https://github.com/arthurvinx/transcriptogramer}
 #'
-#' References: \url{https://doi.org/10.1186/1471-2164-15-1181}, \url{https://doi.org/10.1093/nar/gkq1269}
+#' References: \url{https://doi.org/10.1186/1471-2164-15-1181}, \url{https://doi.org/10.1093/nar/gkq1269}, \url{http://dx.doi.org/10.3390/microorganisms5030049}, \url{https://doi.org/10.1186/s40246-016-0095-x}
 #'
 #' @author
-#' Diego Morais [author, maintainer]
+#' Diego Morais <vinx@ufrn.edu.br> [maintainer]
 #'
-#' Rodrigo Dalmolin [author]
+#' Rodrigo Dalmolin <rodrigo.dalmolin@imd.ufrn.br>
 #'
 "_PACKAGE"
